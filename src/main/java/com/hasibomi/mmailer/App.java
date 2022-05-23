cb7eda6479d8f2b9f528c9fc2c2193ea76b8ac4a
Hasibur Rahman Omi <hrahmanomi@gmail.com>
2022-05-24 00:25:40 +0600

    Initial commit

---
+++ src/main/java/com/hasibomi/mmailer/App.java
@@ -0,0 +1,13 @@
+package com.hasibomi.mmailer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
